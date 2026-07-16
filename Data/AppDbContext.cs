0dd3dfe673632a70d5c5916e7c67b466a560ef7b
Wisani Mabunda <wisanimabunda071@gmail.com>
2026-07-16 20:14:26 +0200

    feat: generate database schema and create sqlite db

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using WimabEventApp.Models;
+
+namespace WimabEventApp.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Guest> Guests => Set<Guest>();
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WimabEventApp.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//SQLite database connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=wimab_event.db"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Wimab Holdings Event API is running!");
 
 app.Run();
